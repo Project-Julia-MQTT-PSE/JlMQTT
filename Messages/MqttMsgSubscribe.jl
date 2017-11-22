7b9fc3beaa2053ed1e195049b56e3a23ea988dbb
UrizielTSD <admin@tsd-esports.de>
2017-11-22 10:23:12 +0100

    Update and fixed

--- Messages/MqttMsgSubscribe.jl
+++ Messages/MqttMsgSubscribe.jl
@@ -14,132 +14,102 @@ messageId = OPTIONAL, only required if QoS level is set to level 1 or 2, to iden
 
 mutable struct MqttMsgSubscribe <: MqttPacket
   msgBase::MqttMsgBase
-  topic::String
-  message::String
-  messageId::UInt8
-	
+  topics::Vector{String}
+  qosLevels::Vector{UInt8}
+
   function MqttMsgSubscribe(
-  topic::String, 
-  message::String; 
-  messageId::UInt8 = 0x00
-  _retain = false,
-  _dup = false,
-  _qos = UInt8(AT_MOST_ONCE))
-
-  this = new()
-  this.msgBase = MqttMsgBase(SUBSCRIBE_TYPE, retain=_retain, dup=_dup, qos=_qos, msgId=messageId)
-  this.message = message
-  this.topic = topic
-  this.messageId = messageId
-
-    return this
+  msgBase::MqttMsgBase,
+  topics::Vector{String},
+  qosLevels::Vector{UInt8})
+  return new(msgBase, topics, qosLevels)
   end
-
 end
-		
+
 function Serialize(msgSubscribe::MqttMsgSubscribe)
-  fixedHeaderSize::Int = 0
+  fixedHeaderSize::Int = 1
   varHeaderSize::Int = 0
   payloadSize::Int = 0
   remainingLength::Int = 0
   index::Int = 1
 
-  msgPackage::Array{UInt8,1}		
-
-  #Check that Topic contain no Wildcards
-  in('#', topic) || in('+', topic) ? throw(ErrorException("Topic can't contain a Wildcard")) : 0x00
-  #Check Topic Length
-  (endof(topic) < MIN_TOPIC_LENGTH || endof(topic) > MAX_TOPIC_LENGTH) ? throw(ErrorException("Topic length exceeded")) : 0x00
-  #Check if QoS Level is wrong
-  if (fixedHeader & QOS_LEVEL_MASK) == QOS_LEVEL_MASK
-    throw(ErrorException("QoS is set to a wrong value"))
+  #topic list empty
+  if size(msgSubscribe.topics) < 1
+    throw(ErrorException("Topic list can't be empty"))
+  end
+  #qos levels list empty
+  if size(msgSubscribe.qosLevels) < 1
+    throw(ErrorException("Qos List can't be empty"))
+  end
+  #topic and qos list lenght must match
+  if size(msgSubscribe.topics) != size(msgSubscribe.qosLevels)
+    throw(ErrorException("Topic & Qos List don't match in size"))
   end
 
-  topicUtf8 = convert(Array{UInt8}, topic)
+  varHeaderSize += MESSAGE_ID_SIZE
 
-  #Topic Length 2 two for MSB & LSB
-  varHeaderSize += endof(topicUtf8) + 2
+  topicIdx::Int = 1
+  topicUtf8 = Array{UInt8}(size(msgSubscribe.topics,))
 
-  #If Qos level is 1 or 2 add the Message ID LSB & MSB
-  if ((fixedHeader & QOS_LEVEL_MASK) == EXACTLY_ONCE || (fixedHeader & QOS_LEVEL_MASK) == 0x04)
-    varHeaderSize += MESSAGE_ID_SIZE
+  while true
+    if topicIdx < size(msgSubscribe.topics)
+      break
+    end
+    #Check topic length
+    if size(msgSubscribe.topics[topicIdx]) < MIN_TOPIC_LENGTH || size(msgSubscribe.topics[topicIdx]) > MAX_TOPIC_LENGTH
+        throw(ErrorException("Topic doesn't match allowed length"))
+    end
+
+    topicUtf8[topicIdx] = convert(Array{UInt8}, msgSubscribe.topics[topicIdx])
+    payloadSize += 2 #topic size (MSB, LSB)
+    payloadSize += size(topicsUtf8[topicIdx])
+    payloadSize += 1 #Qos Byte
+    topicIdx += 1
   end
 
-  if message != ""
-    payloadSize += endof(message)
+  remainingLength += (varHeaderSize + payloadSize)
+
+  tmp::Int = remainingLength
+  #Add Length to Fixed header depending on the remainging length
+  while true
+    fixedHeaderSize += 1
+    tmp = round(tmp / 128)
+    if !(tmp > 0)
+      break
+    end
   end
+  #allocate buffer
+  buffer = Array{UInt8}(fixedHeaderSize + varHeaderSize + payloadSize)
+  #fixed header first
+  buffer[index] = (SUBSCRIBE_TYPE << MSG_TYPE_OFFSET) | SUBSCRIBE_FLAG_BITS
+  index += 1
+  #encode remainingLength
+  index = encodeRemainingLength(remainingLength, buffer, index)
+  #Subscribe use QOS = ! Message ID required
+  if msgSubscribe.msgBase.msgId == 0
+    throw(ErrorException("Wrong message ID"))
+  end
+  buffer[index] = (msgSubscribe.msgBase.msgId >> 8) & 0x00FF #MsB
+  index += 1
+  buffer[index] = msgSubscribe.msgBase.msgId & 0x00FF #LSB
+  index += 1
 
-            #TODO: from subscribe M2MQTT line 192 : recheck this  
- 	    topicIdx::Int = 0;
-            #byte[][] topicsUtf8 = new byte[this.topics.Length][];
-             topicsUtf8::Array{UInt8,2} (count(topic))
-            for (topicIdx = 0; topicIdx < (count(topic); topicIdx+=1)
-                #check topic length
-                if (count(topic[topicInx]) < MIN_TOPIC_LENGTH) || count(topic[topicInx]) > MAX_TOPIC_LENGTH))
-                    ? throw(ErrorException("Topic length exceeded")) : 0x0
-                    #throw new MqttClientException(MqttClientErrorCode.TopicLength)
-				
-                #topicsUtf8[topicIdx] = Encoding.UTF8.GetBytes(this.topics[topicIdx])
-                topicUtf8 = convert(Array{UInt8}, topic[topicIdx])
-                payloadSize += 2; # topic size (MSB, LSB)
-                payloadSize += count(topicsUtf8[topicIdx])
-                payloadSize[index]; # byte for QoS
-                index += 1
-                end
-            end
+  topicIdx = 0
 
-    remainingLength += (varHeaderSize + payloadSize)
-    
-    #building protocol package
-    fixedHeaderSize = 1
-    tmp::Int = remainingLength
-
-        #Add Length to Fixed header depending on the remainging length
-        while true
-          fixedHeaderSize += 1
-          tmp = round(tmp / 128)
-          if !(tmp > 0)
-            break
-          end
-        end
-        msgPackage = Array{UInt8, 1}(fixedHeaderSize + varHeaderSize + payloadSize)
-        
-        #Encode remaining length part for fixed header
-        digit::Int = 0
-        while true
-          digit = mod(remainingLength,UInt8)
-          remainingLength = round(remainingLength / 128)
-          if remainingLength > 0
-            digit = digit | 0x80
-          end
-          msgPackage[index] = convert(UInt8, digit)
-          index += 1
-          remainingLength > 0 ? 0x00 : break
-        end
-        #Move topic name to packageBuffer
-        #First MSB byte
-        msgPackage[index] = (endof(topicUtf8) >> 8) & 0x00FF
-        index += 1
-        #Second LSB byte
-        msgPackage[index] = endof(topicUtf8) & 0x00FF
-        index += 1
-
-        topicIdx::Int = 0;
-        for (topicIdx = 0; topicIdx < (count(topic); topicIdx+=1)
-        
-            #topic name
-            buffer[index] = (byte)((count(topicsUtf8[topicIdx]) >> 8) & 0x00FF) // MSB
-            index += 1
-            buffer[index] = (byte)(count(topicsUtf8[topicIdx]) & 0x00FF) // LSB
-            index += 1
-            Array.Copy(topicsUtf8[topicIdx], 0, buffer, index, count(topicsUtf8[topicIdx]))
-            index += count(topicsUtf8[topicIdx])
-
-            #requested QoS
-            buffer[index] = qosLevels[topicIdx]
-            index += 1
-        end
-        
-        return msgPackage
-        
-        
+  while true
+    if topicIdx < msgSubscribe.topics
+      break
+    end
+      #topic name
+      buffer[index] = (size(topicUtf8[index]) >> 8) & 0x00FF #MSB
+      index += 1
+      buffer[index] = size(topicUtf8[index]) & 0x00FF #LSB
+      index += 1
+      copy!(buffer, index, topicsUtf8[topicIdx], 0, size(topicsUtf8[topicIdx]))
+      index += size(topicsUtf8[topicIdx])
+      #qos
+      buffer[index] = msgSubscribe.qosLevels[topicIdx]
+      index += 1
+      topicIdx += 1
+  end
+  return buffer
+end
