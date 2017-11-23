cc285b4da549d0a0634d8344368d47e976e11f5b
UrizielTSD <admin@tsd-esports.de>
2017-11-23 18:15:21 +0100

    Add files via upload

---
+++ Messages/MqttMsgSuback.jl
@@ -0,0 +1,39 @@
+include("MqttMsgBase.jl")
+include("../MqttNetworkChannel.jl")
+
+mutable struct MqttMsgSuback <: MqttPacket
+  msgBase::MqttMsgBase
+  grantedQosLevels::Vector{UInt8}
+
+  #MqttMsgSuback() = new(MqttMsgBase(SUBACK_TYPE, UInt16(0)), 0, Array{UInt8}(1))
+
+  function MqttMsgSuback(base = MqttMsgBase(SUBACK_TYPE, UInt16(0)), grQosLevels::Vector{UInt8} = Array{UInt8}(5))
+    return new(base, grQosLevels)
+  end
+end
+
+#Parse a SUBACK Message from the inbound stream
+function MsgSubackParse(network::MqttNetworkChannel)
+  index::Int = 1
+  msg::MqttMsgSuback = MqttMsgSuback()
+
+  remainingLength = decodeRemainingLength(network)
+  buffer = Vector{UInt8}(remainingLength)
+  Read(network, buffer)
+  msg.msgBase.msgId = UInt8((buffer[index] << 8) & 0xFF00)
+  index += 1
+  msg.msgBase.msgId |= buffer[index]
+  index += 1
+  msg.grantedQosLevels = Array{UInt8}(remainingLength - MESSAGE_ID_SIZE)
+  qosIdx = 1
+  while true
+    msg.grantedQosLevels[qosIdx] = buffer[index]
+    qosIdx += 1
+    index += 1
+    #POTENTIAL FUTURE ERROR IN THE IF CONDITION
+    if index > remainingLength
+      break
+    end
+  end
+  return msg
+end
