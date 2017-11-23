a7d9d1a0371e3b6901613fffc2fc7769548e7bc5
UrizielTSD <admin@tsd-esports.de>
2017-11-23 18:16:44 +0100

    Update JMqttImplementationTest.jl

--- JMqttImplementationTest.jl
+++ JMqttImplementationTest.jl
@@ -2,4 +2,17 @@
 include("MqttClient.jl")
 
 client = MqttClient()
+@async while true
+  println("WAITING FOR INCOMMING MESSAGE FROM SUBSCRIBED CHANNEL")
+  println(take!(client.subscribedTopicMsgChannel))
+end
 MqttConnect(client, String("Client_1"))
+
+t = Array{String}(2)
+t[1] = "test/1"
+t[2] = "test/2"
+
+q = Array{UInt8}(2)
+q[1] = EXACTLY_ONCE
+q[2] = EXACTLY_ONCE
+MqttSubscribe(client, t, q)
