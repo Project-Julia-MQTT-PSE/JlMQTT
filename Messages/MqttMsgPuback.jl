9f3a6dbdfd4fedef1ae07ec16bb519ed35d65f36
UrizielTSD <admin@tsd-esports.de>
2017-11-26 22:11:07 +0100

    Update MqttMsgPuback.jl

--- Messages/MqttMsgPuback.jl
+++ Messages/MqttMsgPuback.jl
@@ -4,28 +4,26 @@ include("../MqttNetworkChannel.jl")
 
 mutable struct MqttMsgPuback <: MqttPacket
     msgBase::MqttMsgBase
-    messageId::UInt8
-
-    # default constructor
-    MqttMsgPuback() = new(MqttMsgBase(PUBACK_TYPE), 0)
 
     # constructor
-    function MqttMsgPuback(messageId::UInt8; msgBase = MqttMsgBase(PUBACK_TYPE))
-        return new(msgBase, messageId)
+    function MqttMsgPuback(msgBase = MqttMsgBase(PUBACK_TYPE, UInt16(0)))
+        return new(msgBase)
     end # function
 end # struct
 
 # Deserialize MQTT message publish acknowledge
-function Deserialize(msg::MqttMsgPuback, network::MqttNetworkChannel)
+function MsgPubackParse(network::MqttNetworkChannel)
 
     remainingLength::Int = 0
-    buffer::Vector{UInt8}
+    msg::MqttMsgPuback = MqttMsgPuback()
 
     remainingLength = decodeRemainingLength(network)
     buffer = Vector{UInt8}(remainingLength)
-    numberOfBytes = Receive(network, buffer)
-    msg.messageId = (buffer.at(1) << 8) & 0xFF00
-    msg.messageId |= buffer.at(2)
+    Read(network, buffer)
+    msg.msgBase.msgId = (buffer[1] << 8) & 0xFF00
+    msg.msgBase.msgId |= buffer[2]
 
+        println("MESSAGE ID")
+            println(msg.msgBase.msgId)
     return msg
 end
