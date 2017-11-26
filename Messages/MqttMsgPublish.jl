aa91060afa34ee61fef726f0d883f1590095a430
UrizielTSD <admin@tsd-esports.de>
2017-11-26 22:11:39 +0100

    Update MqttMsgPublish.jl

--- Messages/MqttMsgPublish.jl
+++ Messages/MqttMsgPublish.jl
@@ -22,11 +22,11 @@ messageId = OPTIONAL, only required if QoS level is set to level 1 or 2, to iden
 mutable struct MqttMsgPublish <: MqttPacket
   msgBase::MqttMsgBase
   topic::String
-  message
+  message::Vector{UInt8}
 
   function MqttMsgPublish(
     topic::String,
-    message::String;
+    message::Vector{UInt8};
     base = MqttMsgBase(PUBLISH_TYPE, UInt16(0), retain=false, dup=false, qos=AT_MOST_ONCE))
     return new(base, topic, message)
   end
@@ -40,13 +40,11 @@ function Serialize(msgPublish::MqttMsgPublish)
   remainingLength::Int = 0
   index::Int = 1
 
-  msgPackage::Array{UInt8,1}
-
 
   #Check that Topic contain no Wildcards
   in('#', msgPublish.topic) || in('+', msgPublish.topic) ? throw(ErrorException("Topic can't contain a Wildcard")) : 0x00
   #Check Topic Length
-  (endof(msgPublish.topic) < MIN_TOPIC_LENGTH || endof(msgPublish.topic) > MAX_TOPIC_LENGTH) ? throw(ErrorException("Topic length exceeded")) : 0x00
+  (length(msgPublish.topic) < MIN_TOPIC_LENGTH || length(msgPublish.topic) > MAX_TOPIC_LENGTH) ? throw(ErrorException("Topic length exceeded")) : 0x00
   #Check if QoS Level is wrong
   if (msgPublish.msgBase.fixedHeader & QOS_LEVEL_MASK) == QOS_LEVEL_MASK
     throw(ErrorException("QoS is set to a wrong value"))
@@ -55,15 +53,15 @@ function Serialize(msgPublish::MqttMsgPublish)
   topicUtf8 = convert(Array{UInt8}, msgPublish.topic)
 
   #Topic Length 2 two for MSB & LSB
-  varHeaderSize += endof(topicUtf8) + 2
+  varHeaderSize += length(topicUtf8) + 2
 
   #If Qos level is 1 or 2 add the Message ID LSB & MSB
-  if ((msgPublish.msgBase.qos & QOS_LEVEL_MASK) == EXACTLY_ONCE || (msgPublish.msgBase.qos & QOS_LEVEL_MASK) == AT_LEAST_ONCE)
+  if (UInt8(msgPublish.msgBase.qos) == UInt8(EXACTLY_ONCE) || UInt8(msgPublish.msgBase.qos) == UInt8(AT_LEAST_ONCE))
     varHeaderSize += MESSAGE_ID_SIZE
   end
 
-  if message != ""
-    payloadSize += endof(msgPublish.message)
+  if msgPublish.message != ""
+    payloadSize += length(msgPublish.message)
   end
 
   remainingLength += (varHeaderSize + payloadSize)
@@ -80,9 +78,10 @@ function Serialize(msgPublish::MqttMsgPublish)
           break
         end
       end
+
       msgPackage = Array{UInt8, 1}(fixedHeaderSize + varHeaderSize + payloadSize)
 
-      msgPackage[index] = msgPublish.fixedHeader
+      msgPackage[index] = msgPublish.msgBase.fixedHeader
       index += 1
 
       #Encode remaining length part for fixed header
@@ -95,33 +94,26 @@ function Serialize(msgPublish::MqttMsgPublish)
       #Second LSB byte
       msgPackage[index] = endof(topicUtf8) & 0x00FF
       index += 1
-      #Copy Topic to Package
-      p::Int = 1
-      while p <= endof(topicUtf8)
-        msgPackage[index] = topicUtf8[p]
-        index += 1
-        p += 1
-      end
+
+      copy!(msgPackage, index, topicUtf8, 1, length(topicUtf8))
+
+      index += length(topicUtf8)
+
       #Set Message Id if QoS level is Set
       if (msgPublish.msgBase.qos == AT_LEAST_ONCE) || (msgPublish.msgBase.qos == EXACTLY_ONCE)
         #check if messageId isn't 0
-        if msgPublish.msgBase.messageId == 0
+        if msgPublish.msgBase.msgId == 0
           throw(ErrorException("Message Id can not be 0"))
         end
       #Message Id MSB
-      msgPackage[index] = ((msgPublish.msgBase.messageId >> 8) & 0x00FF)
+      msgPackage[index] = ((msgPublish.msgBase.msgId >> 8) & 0x00FF)
       index += 1
       #Message Id LSB
-      msgPackage[index] = (msgPublish.msgBase.messageId & 0x00FF)
+      msgPackage[index] = (msgPublish.msgBase.msgId & 0x00FF)
       index += 1
       end
-      #Copy message to Package
-      p = 1
-      t = convert(Array{UInt8}, msgPublish.message)
-      while p <= endof(t)
-        msgPackage[index] = t[p]
-        index += 1
-        p += 1
+      if msgPublish.message != ""
+            copy!(msgPackage, index,  msgPublish.message, 1, length(msgPublish.message))
       end
       return msgPackage
 end
