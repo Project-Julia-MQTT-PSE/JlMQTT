95eda06c2443ea1cddb4c0bc91246482fce39076
UrizielTSD <admin@tsd-esports.de>
2017-11-26 22:13:44 +0100

    Update MqttMsgUnsubscribe.jl

--- Messages/MqttMsgUnsubscribe.jl
+++ Messages/MqttMsgUnsubscribe.jl
@@ -1,82 +1,86 @@
-module JlMqtt
+include("Definitions.jl")
+include("MqttMsgBase.jl")
+include("../MqttNetworkChannel.jl")
+#const MAX_TOPIC_LENGTH = 65535;
+#const MIN_TOPIC_LENGTH = 1;
+#const MESSAGE_ID_SIZE = 2;
+#const MQTT_MSG_UNSUBSCRIBE_TYPE = 0x0A;
+#const MSG_TYPE_OFFSET = 0x04;
+#const QOS_LEVEL_OFFSET = 0x01;
+#const DUP_FLAG_OFFSET = 0x03;
+#const QOS_LEVEL_AT_LEAST_ONCE = 0x01;
 
-const MAX_TOPIC_LENGTH = 65535;
-const MIN_TOPIC_LENGTH = 1;
-const MESSAGE_ID_SIZE = 2;
-const MQTT_MSG_UNSUBSCRIBE_TYPE = 0x0A;
-const MSG_TYPE_OFFSET = 0x04;
-const QOS_LEVEL_OFFSET = 0x01;
-const DUP_FLAG_OFFSET = 0x03;
-const QOS_LEVEL_AT_LEAST_ONCE = 0x01;
-
-struct MqttMsgUnsubscribe
-  msgPackage::Array{UInt8,1}
-  function MqttMsgUnsubscribe(fixedHeader::UInt8, topic::Array{String,1}, message::String; messageId::UInt8 = 0x00)
-    fixedHeaderSize::Int = 0
-    varHeaderSize::Int = 0
-    payloadSize::Int = 0
-    remainingLength::Int = 0
-    index::Int = 1
-
-    ((count(topic)) == 0) ? throw(ErrorException("No topics present to unsubscribe from.")) : 0x00
-
-    varHeaderSize = MESSAGE_ID_SIZE
-  end
+mutable struct MqttMsgUnsubscribe <: MqttPacket
+    msgBase::MqttMsgBase
+    topics::Vector{String}
+    function MqttMsgUnsubscribe(base::MqttMsgBase = MqttMsgBase(UNSUBACK_TYPE, UInt16(0)), topics::Vector{String} = Vector{String})
+      return new(base, topics)
+    end
 end
 
-    topicsUtf8::Array{byte, 2}
+function Serialize(msgUnsubscribe::MqttMsgUnsubscribe)
+  fixedHeaderSize::Int = 1
+  varHeaderSize::Int = 0
+  payloadSize::Int = 0
+  remainingLength::Int = 0
+  index::Int = 1
 
-    for t in topicsUtf8
+    #topic list empty
+    if length(msgUnsubscribe.topics) < 1
+      throw(ErrorException("Topic list can't be empty"))
+    end
 
-      if endof(topicsUtf8) < MIN_TOPIC_LENGTH || endof(topicsUtf8) > MAX_TOPIC_LENGTH
+    varHeaderSize += MESSAGE_ID_SIZE
 
-        payloadSize += 2
-        payloadSize += topicsUtf8[t]
+    topicIdx::Int = 1
+    topicsUtf8 = Vector{Vector{UInt8}}(length(msgUnsubscribe.topics))
+
+    while topicIdx <= length(msgUnsubscribe.topics)
+      if length(msgUnsubscribe.topics[topicIdx]) < MIN_TOPIC_LENGTH || length(msgUnsubscribe.topics[topicIdx]) > MAX_TOPIC_LENGTH
+        throw(ErrorException("Wrong Topic length!"))
+      end
+
+      topicsUtf8[topicIdx] = convert(Array{UInt8}, msgUnsubscribe.topics[topicIdx])
+      payloadSize += 2
+      payloadSize += length(msgUnsubscribe.topics[topicIdx])
+      topicIdx += 1
+    end
+    remainingLength += (varHeaderSize + payloadSize)
+
+    tmp::Int = remainingLength
+
+    while true
+      fixedHeaderSize += 1
+      tmp = round(tmp / 128)
+      if !(tmp > 0)
+        break
       end
     end
 
-      remainingLength += varHeaderSize + payloadSize
+    #allocate buffer
+    buffer = Array{UInt8}(fixedHeaderSize + varHeaderSize + payloadSize)
+    buffer[index] = (UInt8(UNSUBSCRIBE_TYPE) << MSG_TYPE_OFFSET) | UNSUBSCRIBE_FLAG_BITS
+    index += 1
+    index = encodeRemainingLength(remainingLength, buffer, index)
 
-      fixedHeaderSize = 1
+    if msgUnsubscribe.msgBase.msgId == 0
+      throw(ErrorException("Wrong message ID"))
+    end
 
-      temp::int = remainingLength
-
-      do
-      fixedHeaderSize += 1
-      temp = temp / 128
-      while (temp > 0)
-
-      #allocate buffer for message
-      byte::buffer = fixedHeaderSize + varHeaderSize + payloadSize
-
-      #TODO: qosLevel = QOS_LEVEL_AT_LEAST_ONCE
-
-      buffer[index] = MQTT_MSG_UNSUBSCRIBE_TYPE << MSG_TYPE_OFFSET | qosLevel << QOS_LEVEL_OFFSET
-
-      buffer[index] |= dupFlag ? 1 << DUP_FLAG_OFFSET : 0x00
-      index += 1
-
-      index = encodeRemainingLength(remainingLength, buffer, index)
-
-      # Check if message identifier assigned
-      if(messageId == 0) throw(ErrorException("No message identifier assigned."))
-
-
-      #MSB & LSB
-      buffer[index++] = (messageId >> 8) & 0x00FF
-      buffer[index++] = (messaageId & 0x00FF)
-
-      topicIdx = 0
-
-      for(topicIdx = 0; topicIdx < topics.length; topicIdx++)
-
-        buffer[index += 1] = (topicsUtf8[topicIdx].length >> 8) & 0x00FF) #MSB
-        buffer[index += 1] = (topicsUtf8[topicIdx].length & 0x00FF) #LSB
-
-        # Array.Copy(topicsUtf8[topicIdx], 0, buffer, index, topicsUtf8[topicIdx].Length);
-
-        index += topicsUtf8[topicIdx].Length
-      end
-
-
-      return buffer
+    buffer[index] = (msgUnsubscribe.msgBase.msgId >> 8) & 0x00FF #MsB
+    index += 1
+    buffer[index] = msgUnsubscribe.msgBase.msgId & 0x00FF #LSB
+    index += 1
+    topicIdx = 1
+    while topicIdx <= length(msgUnsubscribe.topics)
+        #topic name
+        buffer[index] = (length(topicsUtf8[topicIdx]) >> 8) & 0x00FF #MSB
+        index += 1
+        buffer[index] = length(topicsUtf8[topicIdx]) & 0x00FF #LSB
+        index += 1
+        copy!(buffer, index, topicsUtf8[topicIdx], 1, length(topicsUtf8[topicIdx]))
+        index += length(topicsUtf8[topicIdx])
+        topicIdx += 1
+    end
+    return buffer
+end
