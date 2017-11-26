39c9b72107c338679fc092666c10c748f3315002
UrizielTSD <admin@tsd-esports.de>
2017-11-26 22:14:07 +0100

    Update JMqttImplementationTest.jl

--- JMqttImplementationTest.jl
+++ JMqttImplementationTest.jl
@@ -1,5 +1,6 @@
 
 include("MqttClient.jl")
+include("Messages/Definitions.jl")
 
 client = MqttClient()
 @async while true
@@ -8,11 +9,23 @@ client = MqttClient()
 end
 MqttConnect(client, String("Client_1"))
 
-t = Array{String}(2)
+t = Array{String}(1)
 t[1] = "test/1"
-t[2] = "test/2"
+#t[2] = "test/2"
 
-q = Array{UInt8}(2)
+m = Array{UInt8}(1)
+#m[1] = "Hello World!"
+
+m = convert(Array{UInt8}, "Hello World!")
+
+q = Array{UInt8}(1)
 q[1] = EXACTLY_ONCE
-q[2] = EXACTLY_ONCE
-MqttSubscribe(client, t, q)
+#q[2] = EXACTLY_ONCE
+#MqttSubscribe(client, t, q)
+#MqttUnsubscribe(client, t)
+
+
+#MqttPublish(client, "test/1", m, qos=AT_LEAST_ONCE)
+#MqttPublish(client, "test/1", m, qos=EXACTLY_ONCE)
+#MqttPublish(client, "test/1", m, qos=AT_LEAST_ONCE)
+MqttDisconnect(client)
