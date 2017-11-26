5e55223a5583dd0afc4a551399c38b569cbc4d83
UrizielTSD <admin@tsd-esports.de>
2017-11-26 22:12:50 +0100

    Add files via upload

---
+++ Messages/MqttMsgUnsuback.jl
@@ -0,0 +1,26 @@
+include("Definitions.jl")
+include("MqttMsgBase.jl")
+include("../MqttNetworkChannel.jl")
+
+mutable struct MqttMsgUnsuback <: MqttPacket
+    msgBase::MqttMsgBase
+    function MqttMsgUnsuback(base::MqttMsgBase = MqttMsgBase(UNSUBACK_TYPE, UInt16(0)))
+      return new(base)
+    end
+end
+
+function MsgUnsubackParse(network::MqttNetworkChannel)
+  index::Int = 1
+  msg::MqttMsgUnsuback = MqttMsgUnsuback()
+
+  remainingLength = decodeRemainingLength(network)
+  buffer = Vector{UInt8}(remainingLength)
+
+  Read(network, buffer)
+
+  msg.msgBase.msgId = (UInt8(buffer[index]) << 8) & 0x00FF
+  index += 1
+  msg.msgBase.msgId |= UInt8(buffer[index])
+
+  return msg
+end
