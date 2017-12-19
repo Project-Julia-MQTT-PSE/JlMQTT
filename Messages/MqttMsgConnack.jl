4cc067ffe4044db41b70ae773c8234d77d149417
UrizielTSD <admin@tsd-esports.de>
2017-12-19 13:55:17 +0100

    Update MqttMsgConnack.jl

--- Messages/MqttMsgConnack.jl
+++ Messages/MqttMsgConnack.jl
@@ -1,4 +1,3 @@
-
 include("MqttMsgBase.jl")
 include("../MqttNetworkChannel.jl")
 
@@ -6,35 +5,14 @@ mutable struct MqttMsgConnack <: MqttPacket
     msgBase::MqttMsgBase
     returnCode::ConnackCode
     sessionPresent::Bool
-
-    # default constructor
-    #MqttMsgConnack() = new(MqttMsgBase(CONNACK_TYPE, UInt16(0)), CONN_ACCEPTED, false)
-
-    # constructor
-    function MqttMsgConnack(returnCode::ConnackCode, sessionPresent::Bool; msgBase::MqttMsgBase = MqttMsgBase(CONNACK_TYPE, UInt16(0)))
-        return new(msgBase, returnCode, sessionPresent)
-    end # function
-end # struct
-
-#=
-# Deserialize MQTT message connack
-# returns a byte array
-function Deserialize(buffer::Array{UInt8, 1})
-
-    if !(length(buffer) > 0) throw(ErrorException("Deserialization error: no data")) end
-
-    msgConnack::MqttMsgConnack = MqttMsgConnack()
-    # check fixed header and remaining length field
-    if (buffer[1] == 0x20) && (buffer[2] == 0x02)
-        msgConnack.sessionPresent = (buffer[3] & 0x01) == 0x01 ? true : false
-        msgConnack.returnCode = buffer[4]
-    end
-
-    return msgConnack
 end
-=#
 
+# constructor
+function MqttMsgConnack(returnCode::ConnackCode, sessionPresent::Bool; msgBase::MqttMsgBase = MqttMsgBase(CONNACK_TYPE, UInt16(0)))
+    return MqttMsgConnack(msgBase, returnCode, sessionPresent)
+end
 # Deserialize MQTT message connack
+#Returns a MqttMsgConnack Package
 function MsgConnackParse(network::MqttNetworkChannel)
 
     remainingLength::Int = 0
