53c885c1d6e2c364e454b8a26ee7179b2471a516
UrizielTSD <admin@tsd-esports.de>
2017-12-19 13:58:08 +0100

    Update MqttMsgPuback.jl

--- Messages/MqttMsgPuback.jl
+++ Messages/MqttMsgPuback.jl
@@ -1,10 +1,9 @@
-
 include("MqttMsgBase.jl")
 include("../MqttNetworkChannel.jl")
 
+#Mqtt Puback Package
 mutable struct MqttMsgPuback <: MqttPacket
     msgBase::MqttMsgBase
-
     # constructor
     function MqttMsgPuback(msgBase = MqttMsgBase(PUBACK_TYPE, UInt16(0)))
         return new(msgBase)
@@ -12,6 +11,7 @@ mutable struct MqttMsgPuback <: MqttPacket
 end # struct
 
 # Deserialize MQTT message publish acknowledge
+#Return a MqttMsgPuback Package
 function MsgPubackParse(network::MqttNetworkChannel)
 
     remainingLength::Int = 0
@@ -23,7 +23,39 @@ function MsgPubackParse(network::MqttNetworkChannel)
     msg.msgBase.msgId = (buffer[1] << 8) & 0xFF00
     msg.msgBase.msgId |= buffer[2]
 
-        println("MESSAGE ID")
-            println(msg.msgBase.msgId)
     return msg
+end
+
+#Serialize a given MqttMsgPuback
+#Return a byte Array
+function Serialize(message::MqttMsgPuback)
+
+    fixedHeaderSize::Int = 1
+    varHeaderSize::Int = 0
+    payloadSize::Int = 0
+    remainingLength::Int = 0
+    index::Int = 1
+
+    varHeaderSize += MESSAGE_ID_SIZE
+    remainingLength += (varHeaderSize + payloadSize)
+
+    tmp::Int = remainingLength
+
+    while true
+      fixedHeaderSize += 1
+      tmp = round(tmp / 128)
+      if !(tmp > 0)
+        break
+      end
+    end
+    buffer = Array{UInt8}(fixedHeaderSize + varHeaderSize + payloadSize)
+    buffer[index] = message.msgBase.fixedHeader
+    index += 1
+
+    index = encodeRemainingLength(remainingLength, buffer, index)
+
+    buffer[index] = (message.msgBase.msgId >> 8) & 0x00FF #MSB
+    index += 1
+    buffer[index] = message.msgBase.msgId & 0x00FF #LSB
+    return buffer
 end
