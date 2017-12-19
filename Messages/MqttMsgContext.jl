296f7510646096df6d69e22c98d931657652e71c
UrizielTSD <admin@tsd-esports.de>
2017-12-19 13:56:05 +0100

    Update MqttMsgContext.jl

--- Messages/MqttMsgContext.jl
+++ Messages/MqttMsgContext.jl
@@ -1,14 +1,16 @@
 include("Definitions.jl")
 include("MqttMsgBase.jl")
+
+#A Context Package which describes the status of an Message Package
 mutable struct MqttMsgContext
   message
   state::MqttMsgState
   flow::MqttMsgFlow
   timestamp::Int
   attempt::Int
+
+  function MqttMsgContext(message, state::MqttMsgState, flow::MqttMsgFlow; timestamp::Int = 1, attempt::Int = 1)
+    return new(message, state, flow, timestamp, attempt)
+  end
 end
-#=
-function MqttMsgContext(message::MqttMsgBase, state::MqttMsgState, flow::MqttMsgFlow, timestamp::Int, attempt::Int)
-  return MqttMsgContext(message, state, flow, timestamp, attempt)
-end
-=#
+
