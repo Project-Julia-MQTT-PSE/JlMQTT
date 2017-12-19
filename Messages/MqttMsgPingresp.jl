77f9694e6ba8b545a8b1bbb8ebccfde7b4a50248
UrizielTSD <admin@tsd-esports.de>
2017-12-19 13:57:34 +0100

    Update MqttMsgPingresp.jl

--- Messages/MqttMsgPingresp.jl
+++ Messages/MqttMsgPingresp.jl
@@ -2,9 +2,9 @@ include("Definitions.jl")
 include("MqttMsgBase.jl")
 include("../MqttNetworkChannel.jl")
 
+#Nqtt Ping response package
 mutable struct MqttMsgPingresp <: MqttPacket
     msgBase::MqttMsgBase
-
     # constructor
     function MqttMsgPingresp(msgBase = MqttMsgBase(PINGRESP_TYPE, UInt16(0)))
         return new(msgBase)
@@ -12,12 +12,10 @@ mutable struct MqttMsgPingresp <: MqttPacket
 end # struct
 
 # Deserialize MQTT message ping response
+#Return an MqttMsgPingresp package
 function MsgPingrespParse(network::MqttNetworkChannel)
-
     msg::MqttMsgPingresp = MqttMsgPingresp()
     remainingLength::Int = 0
-
     remainingLength = decodeRemainingLength(network)
-
     return msg
 end
