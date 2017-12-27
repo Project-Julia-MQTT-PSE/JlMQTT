4735d4b0699e414f9e1c6c9f7b1b8c989b18fbe7
UrizielTSD <admin@tsd-esports.de>
2017-12-27 13:51:39 +0100

    Update MqttMsgDisconnect.jl

--- Messages/MqttMsgDisconnect.jl
+++ Messages/MqttMsgDisconnect.jl
@@ -5,20 +5,18 @@ include("../MqttNetworkChannel.jl")
 #Mqtt Disconnect packages
 mutable struct MqttMsgDisconnect <: MqttPacket
     msgBase::MqttMsgBase
+end
 
-
-  function MqttMsgDisconnect(base::MqttMsgBase = MqttMsgBase(DISCONNECT_TYPE, UInt16(0)))
-    return new(base)
-  end
-end # struct
+#Disconnect package constructor
+function MqttMsgDisconnectConstructor(base::MqttMsgBase = MqttMsgBase(DISCONNECT_TYPE, UInt16(0)))
+  return MqttMsgDisconnect(base)
+end
 
 # Serialize MQTT message disconnect
 # returns a byte array
 function Serialize(msg::MqttMsgDisconnect)
-
     buffer = Array{UInt8, 1}(2)
     buffer[1] = msg.msgBase.fixedHeader
-    buffer[2] = UInt8(0) #reaminingLength field
-
+    buffer[2] = UInt8(0)
     return buffer
 end
