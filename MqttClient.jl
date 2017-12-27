06b1d110f551b0525862976b586fb6436aea1ae4
UrizielTSD <admin@tsd-esports.de>
2017-12-27 13:57:21 +0100

    Update MqttClient.jl

--- MqttClient.jl
+++ MqttClient.jl
@@ -4,58 +4,43 @@ include("MqttNetworkChannel.jl")
 include("Messages/MqttMsgConnect.jl")
 include("ReceivedMessage.jl")
 
+#Session struct
+#UNUSED
 mutable struct MqttSession
     clientId::String
     inFlightMessages::Dict
 end
 
-#Mqtt Client Package, stores all relevant variable to run the interface
+#Client, stores all relevant variable to run the interface
 mutable struct MqttClient
-    channel::MqttNetworkChannel
-    lastCommTime::Int
-    isRunning::Bool
-    session::MqttSession
-    clientId::String
-    cleanSession::Bool
-    will::WillOptions
-    willFlag::Bool
-    keepAlivePeriod::Int
-    staticMsgId::UInt16
-    sendReceiveChannel
-    subscribedTopicMsgChannel
-    contextMsgChannel
-    contextQueueLock
-    function MqttClient(
-      channel::MqttNetworkChannel = MqttNetworkChannel(TCPSocket(), "test.mosquitto.org", 1883),
-      lastCommTime::Int = 0,
-      isRunning::Bool = true,
-      session::MqttSession = MqttSession(String(""), Dict()),
-      clientId::String = String(""),
-      cleanSession::Bool = true,
-      will::WillOptions = WillOptions(),
-      willFlag::Bool = false,
-      keepAlivePeriod::Int = 0,
-      staticMsgId::UInt16 = UInt16(1),
-      sendReceiveChannel::Channel{Any} = Channel{Any}(1),
-      subscribedTopicMsgChannel::Channel{ReceivedMessage} = Channel{ReceivedMessage}(10),
-      contextMsgChannel::Channel{Any} = Channel{Any}(30),
-      contextQueueLock = Threads.Mutex())
+  channel::MqttNetworkChannel
+  lastCommTime::Int
+  isRunning::Bool
+  session::MqttSession
+  clientId::String
+  keepAlivePeriod::UInt16
+  staticMsgId::UInt16
+  sendReceiveChannel
+  subscribedTopicMsgChannel
+  contextMsgChannel
+  contextQueueLock
+end
 
-      this = new()
-      this.channel = channel
-      this.lastCommTime = lastCommTime
-      this.isRunning = isRunning
-      this.session = session
-      this.clientId = clientId
-      this.cleanSession = cleanSession
-      this.will = will
-      this.willFlag = willFlag
-      this.keepAlivePeriod = keepAlivePeriod
-      this.staticMsgId = staticMsgId
-      this.sendReceiveChannel = sendReceiveChannel
-      this.subscribedTopicMsgChannel = subscribedTopicMsgChannel
-      this.contextMsgChannel = contextMsgChannel
-      this.contextQueueLock = contextQueueLock
-      return this
-    end
+#Client constructor
+function MqttClientConstructor(
+  channel::MqttNetworkChannel = MqttNetworkChannel(TCPSocket(), "test.mosquitto.org", 1883),
+  lastCommTime::Int = 0,
+  isRunning::Bool = true,
+  session::MqttSession = MqttSession(String(""), Dict()),
+  clientId::String = String(""),
+  keepAlivePeriod::UInt16 = UInt16(KEEP_ALIVE_PERIOD_DEFAULT),
+  staticMsgId::UInt16 = UInt16(1),
+  sendReceiveChannel::Channel{Any} = Channel{Any}(1),
+  subscribedTopicMsgChannel::Channel{ReceivedMessage} = Channel{ReceivedMessage}(10),
+  contextMsgChannel::Channel{Any} = Channel{Any}(30),
+  contextQueueLock = Threads.Mutex())
+
+  return MqttClient(channel, lastCommTime, isRunning, session, clientId, keepAlivePeriod, staticMsgId,
+  sendReceiveChannel, subscribedTopicMsgChannel, contextMsgChannel, contextQueueLock)
+  return a
 end
