0c5ad340a29f2f41d2547128c47572183a8bc073
UrizielTSD <admin@tsd-esports.de>
2017-12-27 13:51:22 +0100

    Update MqttMsgContext.jl

--- Messages/MqttMsgContext.jl
+++ Messages/MqttMsgContext.jl
@@ -8,9 +8,9 @@ mutable struct MqttMsgContext
   flow::MqttMsgFlow
   timestamp::Int
   attempt::Int
-
-  function MqttMsgContext(message, state::MqttMsgState, flow::MqttMsgFlow; timestamp::Int = 1, attempt::Int = 1)
-    return new(message, state, flow, timestamp, attempt)
-  end
 end
 
+#Message Context Constructor
+function MqttMsgContextConstructor(message, state::MqttMsgState, flow::MqttMsgFlow; timestamp::Int = 1, attempt::Int = 1)
+  return MqttMsgContext(message, state, flow, timestamp, attempt)
+end
