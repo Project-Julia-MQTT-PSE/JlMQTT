1b66dda8ea7268b16fc6c5febd36fe52b428e41f
UrizielTSD <admin@tsd-esports.de>
2017-12-27 13:52:04 +0100

    Update MqttMsgPingreq.jl

--- Messages/MqttMsgPingreq.jl
+++ Messages/MqttMsgPingreq.jl
@@ -5,19 +5,18 @@ include("../MqttNetworkChannel.jl")
 #Mqtt Ping request package
 mutable struct MqttMsgPingreq <: MqttPacket
     msgBase::MqttMsgBase
-    # default constructor
-  function MqttMsgPingreq(base::MqttMsgBase = MqttMsgBase(PINGREQ_TYPE, UInt16(0)))
-    return new(base)
-  end
-end # struct
+end
+
+# Ping request constructor
+function MqttMsgPingreqConstructor(base::MqttMsgBase = MqttMsgBase(PINGREQ_TYPE, UInt16(0)))
+return MqttMsgPingreq(base)
+end
 
 # Serialize MQTT message ping request
 # returns a byte array
 function Serialize(msg::MqttMsgPingreq)
-
     buffer = Array{UInt8, 1}(2)
     buffer[1] = msg.msgBase.fixedHeader
-    buffer[2] = UInt8(0) #reaminingLength field
-
+    buffer[2] = UInt8(0)
     return buffer
 end
