f08437de7c9d76ecdb7dc4005ef9008008355038
UrizielTSD <admin@tsd-esports.de>
2017-12-27 13:53:37 +0100

    Update MqttMsgPublish.jl

--- Messages/MqttMsgPublish.jl
+++ Messages/MqttMsgPublish.jl
@@ -7,36 +7,19 @@ const MIN_TOPIC_LENGTH = 1;
 const MESSAGE_ID_SIZE = 2;
 const QOS_LEVEL_MASK = 0x06
 
-#=Represent a PUBLISH Package which can be send to the Broker
-
-fixedHeader = the FixedHeader value
-
-topic = topic to which the message will be published
-
-message = to be published message
-
-messageId = OPTIONAL, only required if QoS level is set to level 1 or 2, to identify a specific message
-
-=#
-
-#Structure generates a publish package which is sent to the broker.
-#msgBase::MqttMsgBase' : Message base instance used in creation of package.
-#topic::String' : The topic message is published to in string form.
-#message:Vector{UInt8}' : The message itself that is published to the topic and will be sent to all subscribers
-
 #Mqtt Publish Package
 mutable struct MqttMsgPublish <: MqttPacket
   msgBase::MqttMsgBase
   topic::String
   message::Vector{UInt8}
+end
 
-  function MqttMsgPublish(
-    topic::String;
-    message::Vector{UInt8} = Vector{UInt8}(1),
-    base = MqttMsgBase(PUBLISH_TYPE, UInt16(0), retain=false, dup=false, qos=AT_MOST_ONCE))
-    return new(base, topic, message)
-  end
-
+#Publish package constructor
+function MqttMsgPublishConstructor(
+  topic::String;
+  message::Vector{UInt8} = Vector{UInt8}(1),
+  base = MqttMsgBase(PUBLISH_TYPE, UInt16(0), retain=false, dup=false, qos=AT_MOST_ONCE))
+  return MqttMsgPublish(base, topic, message)
 end
 
 #Serializes the publish message.
@@ -56,73 +39,59 @@ function Serialize(msgPublish::MqttMsgPublish)
   if (msgPublish.msgBase.fixedHeader & QOS_LEVEL_MASK) == QOS_LEVEL_MASK
     throw(ErrorException("QoS is set to a wrong value"))
   end
-
   topicUtf8 = convert(Array{UInt8}, msgPublish.topic)
-
   #Topic Length 2 two for MSB & LSB
   varHeaderSize += length(topicUtf8) + 2
-
   #If Qos level is 1 or 2 add the Message ID LSB & MSB
-  if (UInt8(msgPublish.msgBase.qos) == UInt8(EXACTLY_ONCE) || UInt8(msgPublish.msgBase.qos) == UInt8(AT_LEAST_ONCE))
+  qos = QosLevel(UInt8((msgPublish.msgBase.fixedHeader & QOS_LEVEL_MASK) >> QOS_LEVEL_OFFSET))
+  if any(qos .== (EXACTLY_ONCE, AT_LEAST_ONCE))
     varHeaderSize += MESSAGE_ID_SIZE
+      #check if messageId isn't 0
+      if msgPublish.msgBase.msgId == 0
+        throw(ErrorException("Message Id can not be 0"))
+      end
   end
-
   if msgPublish.message != ""
     payloadSize += length(msgPublish.message)
   end
-
   remainingLength += (varHeaderSize + payloadSize)
-
   #building protocol package
   fixedHeaderSize = 1
   tmp::Int = remainingLength
-
-      #Add Length to Fixed header depending on the remainging length
-      while true
-        fixedHeaderSize += 1
-        tmp = round(tmp / 128)
-        if !(tmp > 0)
-          break
-        end
-      end
-
-      msgPackage = Array{UInt8, 1}(fixedHeaderSize + varHeaderSize + payloadSize)
-
-      msgPackage[index] = msgPublish.msgBase.fixedHeader
-      index += 1
-
-      #Encode remaining length part for fixed header
-      index = encodeRemainingLength(remainingLength, msgPackage, index)
-
-      #Move topic name to packageBuffer
-      #First MSB byte
-      msgPackage[index] = (endof(topicUtf8) >> 8) & 0x00FF
-      index += 1
-      #Second LSB byte
-      msgPackage[index] = endof(topicUtf8) & 0x00FF
-      index += 1
-
-      copy!(msgPackage, index, topicUtf8, 1, length(topicUtf8))
-
-      index += length(topicUtf8)
-
-      #Set Message Id if QoS level is Set
-      if (msgPublish.msgBase.qos == AT_LEAST_ONCE) || (msgPublish.msgBase.qos == EXACTLY_ONCE)
-        #check if messageId isn't 0
-        if msgPublish.msgBase.msgId == 0
-          throw(ErrorException("Message Id can not be 0"))
-        end
-      #Message Id MSB
-      msgPackage[index] = ((msgPublish.msgBase.msgId >> 8) & 0x00FF)
-      index += 1
-      #Message Id LSB
-      msgPackage[index] = (msgPublish.msgBase.msgId & 0x00FF)
-      index += 1
-      end
-      if msgPublish.message != ""
-            copy!(msgPackage, index,  msgPublish.message, 1, length(msgPublish.message))
-      end
-      return msgPackage
+  #Add Length to Fixed header depending on the remainging length
+  while true
+    fixedHeaderSize += 1
+    tmp = round(tmp / 128)
+    if !(tmp > 0)
+      break
+    end
+  end
+  msgPackage = Array{UInt8, 1}(fixedHeaderSize + varHeaderSize + payloadSize)
+  msgPackage[index] = msgPublish.msgBase.fixedHeader
+  index += 1
+  #Encode remaining length part for fixed header
+  index = encodeRemainingLength(remainingLength, msgPackage, index)
+  #Move topic name to packageBuffer
+  #First MSB byte
+  msgPackage[index] = (endof(topicUtf8) >> 8) & 0x00FF
+  index += 1
+  #Second LSB byte
+  msgPackage[index] = endof(topicUtf8) & 0x00FF
+  index += 1
+  copy!(msgPackage, index, topicUtf8, 1, length(topicUtf8))
+  index += length(topicUtf8)
+  if any(qos .== (EXACTLY_ONCE, AT_LEAST_ONCE))
+    #Message Id MSB
+    msgPackage[index] = ((msgPublish.msgBase.msgId >> 8) & 0x00FF)
+    index += 1
+    #Message Id LSB
+    msgPackage[index] = (msgPublish.msgBase.msgId & 0x00FF)
+    index += 1
+  end
+  if msgPublish.message != ""
+    copy!(msgPackage, index,  msgPublish.message, 1, length(msgPublish.message))
+  end
+  return msgPackage
 end
 
 
@@ -130,7 +99,7 @@ end
 #REturn a MqttMsgPub Packag
 function MsgPublishParse(network::MqttNetworkChannel, fixedHeaderFirstByte::UInt8)
   index::Int = 1
-  msg::MqttMsgPublish = MqttMsgPublish("")
+  msg::MqttMsgPublish = MqttMsgPublishConstructor("")
 
   #Allocate Buffer
   remainingLength::Int = decodeRemainingLength(network)
@@ -148,19 +117,16 @@ function MsgPublishParse(network::MqttNetworkChannel, fixedHeaderFirstByte::UInt
 
   #Save topic
   msg.topic = topicUtf8
-  #QoS level from fixed header
-  msg.msgBase.qos = UInt8((fixedHeaderFirstByte &  QOS_LEVEL_MASK) >> QOS_LEVEL_OFFSET)
+  #Save fixedheader
+  msg.msgBase.fixedHeader = fixedHeaderFirstByte
   #Check for wrong QoS
-  if msg.msgBase.qos > EXACTLY_ONCE
+  qos = QosLevel(UInt8((msg.msgBase.fixedHeader & QOS_LEVEL_MASK) >> QOS_LEVEL_OFFSET))
+  if qos > EXACTLY_ONCE
     throw(ErrorException("QOS LEVEL NOT ALLOWED!"))
   end
-  #Read DUP flag
-  msg.msgBase.dup = (((fixedHeaderFirstByte & DUP_FLAG_MASK) >> DUP_FLAG_OFFSET) == 0x01)
-  #Read Retain flag
-  msg.msgBase.retain = (((fixedHeaderFirstByte & RETAIN_FLAG_MASK) >> RETAIN_FLAG_OFFSET) == 0x01)
 
   #Message id is only valid with QoS lvl1&2
-  if ((msg.msgBase.qos == AT_LEAST_ONCE) || msg.msgBase.qos == EXACTLY_ONCE)
+  if any(qos .== (AT_LEAST_ONCE, EXACTLY_ONCE))
     msg.msgBase.msgId = (buffer[index] << 8) & 0xFF00
     index += 1
     msg.msgBase.msgId |= buffer[index]
