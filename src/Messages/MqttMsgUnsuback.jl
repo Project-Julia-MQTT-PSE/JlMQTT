7ce9f069975301b50626a3321f54afa0ecff390c
catherinegracekane <catherinegkane@gmail.com>
2018-01-23 16:35:08 +0100

    unsuback documentation

--- src/Messages/MqttMsgUnsuback.jl
+++ src/Messages/MqttMsgUnsuback.jl
@@ -2,16 +2,52 @@ include("Definitions.jl")
 include("MqttMsgBase.jl")
 include("../MqttNetworkChannel.jl")
 
+"""
+JlMQTT.MqttMsgUnsuback
+
+This is the Unsubscribe Acknowledgement Package 
+
+"""
 #Mqtt unsuback Package
 mutable struct MqttMsgUnsuback <: MqttPacket
     msgBase::MqttMsgBase
 end
+
+"""
+ JlMQTT.MqttMsgUnsubackConstructor(base::MqttMsgBase = MqttMsgBase(UNSUBACK_TYPE, UInt16(0)))
+
+ ## Parameters:
+ \nbase - [in] type ['MqttMsgBase'](@ref)
+ 
+ ## Returns:
+ \n[out]  ['MqttMsgUnsuback'](@ref)
+ 
+ """
 
 #Unsuback package constructor
 function MqttMsgUnsubackConstructor(base::MqttMsgBase = MqttMsgBase(UNSUBACK_TYPE, UInt16(0)))
   return MqttMsgUnsuback(base)
 end
 
+"""
+ JlMQTT.MsgUnsubackParse(network::MqttNetworkChannel)
+  index::Int = 1
+  msg::MqttMsgUnsuback = MqttMsgUnsubackConstructor()
+
+  remainingLength = decodeRemainingLength(network)
+  buffer = Vector{UInt8}(remainingLength)
+
+ ## Parameters:
+ \nnetwork - [in] type ['MqttNetworkChannel'](@ref)
+ \nindex - [optional] type Int
+ \nmsg - [optional] type ['MqttMsgUnsuback'](@ref)
+ \nremainingLength - [optional] type ['decodeRemainingLength'](@ref)
+ \nbuffer - [optional] type Vector{UInt8}
+ 
+ ## Returns:
+ \n[out]  msg['MqttMsgUnsuback'](@ref)
+ 
+ """
 # Deserialize MQTT message unsuback
 #REturn a MqttMsgUnsuback Package
 function MsgUnsubackParse(network::MqttNetworkChannel)
